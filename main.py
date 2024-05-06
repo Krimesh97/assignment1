b555f5c4a8f72657c9cfaa98791af4aef5df78a5
Krimesh Bajimayo <krimeshbajimayo@gmail.com>
2024-05-06 11:56:54 +0545

    database queries and updates

--- main.py
+++ main.py
@@ -14,19 +14,21 @@ def main(sys_argv):
 
     dictionary_filepath = config['PATHS']['input_dictionary']
     output_folder = config['PATHS']['output_directory']
+    directory_candidates = config['module']['directory_candidates']
     db_name = config['Database']['database_name']
     table_name = config['Database']['table_name']
 
     word_list = get_word_list(dictionary_filepath)
     print("Success in Reading Dictionary File")
 
-    curated = create_files_and_directories(word_list, output_folder)
+    directory_candidates = list(directory_candidates)
+    curated = create_files_and_directories(word_list, output_folder, directory_candidates)
     print("Success in Creating Required Files and Directories")
 
     print("Compressing Files and Reporting Compression Ratios")
     apply_compression_pipeline(output_folder)
 
-    # apply_db_pipeline(db_file=db_name, table_name=table_name, word_list=word_list)
+    apply_db_pipeline(db_file=db_name, table_name=table_name, word_list=word_list)
 
 
 if __name__ == "__main__":
--- module/database.py
+++ module/database.py
@@ -1,53 +1,54 @@
 import sqlite3
+import os
 
 
-def setup_db(db_file, table_name):
+def setup_connection(db_file: str | os.PathLike) -> sqlite3.Connection:
     """
-    create a database connection to a SQLite database
-    :param db_file: database file
-    :param table_name: Table name for insertion of words
+    Sets up Database and returns connection to it
+    :param db_file: path to database file
+    :return: db connection object
     """
     db_connection = None
     try:
         db_connection = sqlite3.connect(db_file)
         print("Connection to db successful:", sqlite3.version)
+    except sqlite3.Error as error:
+        print('Failed to setup database:', error)
+    return db_connection
 
+
+def setup_tables(db_connection: sqlite3.Connection, table_name: str):
+    """
+    creates required tables
+    :param db_connection: connection to database
+    :param table_name: Table name for insertion of words
+    """
+    try:
         # cursor object
         cursor_obj = db_connection.cursor()
-
-        # Drop the GEEK table if already exists.
         cursor_obj.execute("DROP TABLE IF EXISTS {}".format(table_name))
-
         table = """
                     CREATE TABLE {} (
                     Word_id INTEGER PRIMARY KEY AUTOINCREMENT,
                     Word CHAR(512) NOT NULL
                    );
                 """.format(table_name)
-
         cursor_obj.execute(table)
         print("Table is Ready")
 
     except sqlite3.Error as error:
         print('Failed to setup database:', error)
-    finally:
-        if db_connection:
-            db_connection.close()
 
 
-def insert_word_records(db_file, table_name, word_list):
+def insert_word_records(db_connection: sqlite3.Connection, table_name: str, word_list: list | tuple):
     """
-    Insert Word Records into table
-    :param db_file: database file
+    Inserts Word Records into table
+    :param db_connection: connection to database
     :param table_name: Table name for insertion of words
     :param word_list: Dictionary word list to be added to table
     """
-    db_connection = None
     try:
-        db_connection = sqlite3.connect(db_file)
         cursor_obj = db_connection.cursor()
-        # print("Connected to SQLite")
-
         for word in word_list:
             word = word.lower()
             sqlite_insert_with_param = """INSERT INTO {} (Word) VALUES (?);""".format(table_name)
@@ -59,11 +60,36 @@ def insert_word_records(db_file, table_name, word_list):
 
     except sqlite3.Error as error:
         print("Failed to insert Dictionary Word into sqlite table:", error)
-    finally:
-        if db_connection:
-            db_connection.close()
-            print("The SQLite connection is closed")
 
+
+def run_analytics_simple(word_list: list | tuple):
+    num_length_GT_5 = 0
+    num_has_dup_chars = 0
+    num_same_start_end = 0
+    for word in word_list:
+        num_length_GT_5 += int(len(word) > 5)
+        num_has_dup_chars += int(len(word) > len(set(word)))
+        num_same_start_end += int(word[0] == word)
+    return num_length_GT_5, num_has_dup_chars, num_same_start_end
+
+
+def update_table_make_upper(db_connection: sqlite3.Connection, table_name: str):
+    """
+    Updates Words to UPPER case in target table
+    :param db_connection: connection to database
+    :param table_name: Table name for insertion of words
+
+    """
+    try:
+        cursor_obj = db_connection.cursor()
+        update_statement = """UPDATE {} SET Word = UPPER(Word);""".format(table_name)
+        cursor_obj.execute(update_statement)
+        db_connection.commit()
+        print("Successfully updated words to upper case")
+        cursor_obj.close()
+
+    except sqlite3.Error as error:
+        print("Failed to update table:", error)
 
 
 def apply_db_pipeline(db_file: str, table_name: str, word_list: [list, tuple]):
@@ -73,5 +99,19 @@ def apply_db_pipeline(db_file: str, table_name: str, word_list: [list, tuple]):
     :param table_name: Table name for insertion of words
     :param word_list: Dictionary word list to be added to table
     """
-    setup_db(db_file, table_name)
-    insert_word_records(db_file, table_name, word_list)
+
+    db_connection = setup_connection(db_file)
+    if db_connection is None:
+        print("Skipping Database Operations as connection was not established")
+        return
+    setup_tables(db_connection, table_name)
+    insert_word_records(db_connection, table_name, word_list)
+    num_length_GT_5, num_has_dup_chars, num_same_start_end = run_analytics_simple(word_list)
+
+    print("The number of words with more than 5 chars: ", num_length_GT_5)
+    print("The number of words with duplicate chars: ", num_has_dup_chars)
+    print("The number of words with same start and end chars: ", num_same_start_end)
+    update_table_make_upper(db_connection, table_name)
+
+    if db_connection:
+        db_connection.close()
